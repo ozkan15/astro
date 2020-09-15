cab723cd65c5f6635c438d424da19b28a8ed0bb8
Mehmet Akif Özkan <akifozkan_@hotmail.com>
2020-09-15 19:56:49 +0300

    login layout added

--- app/src/main/java/com/example/astro/MainActivity.java
+++ app/src/main/java/com/example/astro/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
